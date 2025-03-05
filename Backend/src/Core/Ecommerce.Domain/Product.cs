add57664cb8f2074602f7fe1aab014bd1cb278a2
Javier Rivera <ingjrz@gmail.com>
2025-03-05 09:15:32 -0600

    Created Entities for Product and other dependencies.

--- Backend/src/Core/Ecommerce.Domain/Product.cs
+++ Backend/src/Core/Ecommerce.Domain/Product.cs
@@ -1,21 +1,24 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Ecommerce.Domain.Common;
 
 namespace Ecommerce.Domain;
 
-    public class Product : BaseDomainModel {
-        public string? Nombre { get; set; }
+public class Product : BaseDomainModel
+{
+    [Column(TypeName = "NVARCHAR(100)")]
+    public string? Nombre { get; set; }
+    [Column(TypeName = "DECIMAL(10,2)")]
+    public decimal Precio { get; set; }
+    [Column(TypeName = "NVARCHAR(4000)")]
+    public string? Descripcion { get; set; }
 
-        public decimal Precio { get; set; }
-        
-        public string? Descripcion { get; set; }
+    public int Rating { get; set; }
+    [Column(TypeName = "NVARCHAR(100)")]
+    public string? Vendedor { get; set; }
 
-        public int Rating { get; set; }
+    public int Stock { get; set; }
 
-        public string? Vendedor { get; set; }
+    public ProductStatus Status { get; set; } = ProductStatus.Activo;
 
-        public int Stock { get; set; }
-
-        public ProductStatus Status { get; set; }
-
-        public int CategoryId { get; set; }
-    }
+    public int CategoryId { get; set; }
+}
