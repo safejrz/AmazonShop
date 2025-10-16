07a90da16925b9ff65875bfe52a8eed334c515b7
Javier Rivera <ingjrz@gmail.com>
2025-10-16 13:10:17 -0600

    Adding entities rules.

--- Backend/src/Core/Ecommerce.Domain/OrderItem.cs
+++ Backend/src/Core/Ecommerce.Domain/OrderItem.cs
@@ -6,7 +6,7 @@ namespace Ecommerce.Domain;
 public class OrderItem : BaseDomainModel{
     public Product? Product { get; set; }
     public int ProductId { get; set; }  
-    [Column(TypeName = "decimal(10,2)")]
+    
     public decimal Precio{ get; set; }
 
     public int Cantidad { get; set; }
