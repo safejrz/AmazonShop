b5659e041c2511c1ec03eee586eda40994b280df
Javier Rivera <ingjrz@gmail.com>
2026-01-16 16:14:34 -0600

    Remove duplicated files.

--- backend_all/src/Core/Ecommerce.Application/Models/Authorization/Role.cs
+++
@@ -1,7 +0,0 @@
-namespace Ecommerce.Application.Models.Authorization;
-
-public static class Role
-{
-    public const string ADMIN = nameof(ADMIN);
-    public const string USER = nameof(USER);
-}
--- backend_all/src/Core/Ecommerce.Domain/AppRole.cs
+++
@@ -1,6 +0,0 @@
-namespace Ecommerce.Domain;
-
-public static class AppRole {
-    public static readonly string GenericUser = "USER";
-    public static readonly string Admin = "ADMIN";
-}
