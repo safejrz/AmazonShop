984543189811e71ef9eebd8e1469e554e1ae598d
Javier Rivera <ingjrz@gmail.com>
2026-03-02 16:24:10 -0600

    Retry logic apps test

--- Backend/src/Core/Ecommerce.Application/Contracts/Infrastructure/IManageImageService.cs
+++ Backend/src/Core/Ecommerce.Application/Contracts/Infrastructure/IManageImageService.cs
@@ -3,5 +3,5 @@ using Ecommerce.Application.Models.ImageManagement;
 namespace Ecommerce.Application.Contracts.Infrastructure;
 public interface IManageImageService
 {
- Task<ImageResponse> UploadImage(ImageData imageStream);
+ Task<ImageResponse> UploadImage(ImageData imageStream); 
 }
